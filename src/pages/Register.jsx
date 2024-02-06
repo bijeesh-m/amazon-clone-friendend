90489ececdb73a085c2d4118a213293bd60ab0f5
Bijeesh M <bijeeshh.m@gmail.com>
2024-02-06 17:05:30 +0530

    fix: fixed the error

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -98,6 +98,8 @@ const Register = () => {
       });
   };
 
+  console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+
   const verifyOtp = () => {
     const vToastId = toast.loading("Verifing OTP...");
     axios
